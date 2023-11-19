347c7a970f3d31b6b0a571d8d4627ff9e00e471e
Mastermintermt <150274518+Mastermintermt@users.noreply.github.com>
2023-11-19 18:40:52 +0530

    Update route.py

--- route.py
+++ route.py
@@ -6,8 +6,7 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
     return web.json_response("PyʀᴏBᴏᴛᴢ")
 
-
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
+    web_app = web.Application(client_max_size=50000000)  # Updated to 50 MB
     web_app.add_routes(routes)
     return web_app
